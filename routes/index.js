dd52398ede3629fdf31c42194770638e67c2bb73
mangomateo <velarde.matthew@gmail.com>
2021-06-17 16:19:59 -0700

    log database contents to index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models/').Book;
+
+/**
+ * Try/Catch handler function 
+**/
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  res.json(books);
+}));
 
 module.exports = router;
